d9366dbd722a051bf0000b2aabb06bb912abbc51
Simone Baracchi <simonebaracchi@gmail.com>
2018-07-11 23:19:22 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+=== chordexport ===
+
+This is a simple tool to import chords (or similarly formatted HTML?) and show them in the UNIX shell and/or make them more edit-friendly.
+
+More precisely: it reformats HTML when text and other super-positioned text (<sup>) are used together, by splitting text and super-positioned stuff on two separate lines.
+
+== Usage ==
+
+./chordexport.sh <url>
---
+++ export.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+
+wget $1 -O /tmp/tab 2>/dev/null
+
+xmllint --nocdata --xpath '//*[@id="xte-view"]' --html /tmp/tab >/tmp/parsed 2>/dev/null
+
+# set separator to null so that spaces in strings are preserved
+IFS=''
+divcount=0
+while true; do
+    divcount=$(( divcount + 1 ))
+        
+    # extract a verse (a group of lines)
+    xmllint --xpath "//div[1]/div[$divcount]" --html /tmp/parsed | sed 's/<div class="song">//;s/<\/div>//' > /tmp/verse 2>/dev/null
+    if [ ! -s /tmp/verse ]; then
+        exit
+    fi
+    # cycle through single lines
+    while read line; do
+        #separate all chords on newlines for easier parsing
+        echo $line | sed 's/<sup>/\n<sup>/g ; s/<\/sup>/<\/sup>\n/g' > /tmp/words
+        # load one line at a time; if it is not a chord, print spaces, print chords otherwise
+        while read words; do
+            echo $words | tr -d '\n' | sed -E '
+                /^<sup>/!        s/./\x1b[1C/g;
+                /^<sup>/         s/<sup>(.*)<\/sup>/\x1b[s\1\x1b[u/g;
+                '
+        done < /tmp/words
+        # newline
+        echo
+        # load one line at a time, but this time print the lyrics, and ignore the chords
+        while read words; do
+            echo $words | tr -d '\n' | sed -E 's/<sup>.*<\/sup>//g'
+        done < /tmp/words
+        echo
+    done < /tmp/verse
+done
